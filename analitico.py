794e637896351042604fda4fc97f6a3940a5a181
Rafael Albuquerque <rafael.silva.albuquerque@hotmail.com>
2025-09-09 21:16:15 -0300

    formula corrigida

--- analitico.py
+++ analitico.py
@@ -13,10 +13,10 @@ def disponibilidade(
     p - prob de um servidor estar up, 0 <= p <= 1
     X - número de servidores disponíveis em um dado momento
     """
-    prob = 0
-    for i in range(n - k):
-        prob += math.comb(n, k + i) * p ** (k + i) * (1 - p) ** (n - k + i)
-    return 1.0 - prob
+    prob = 0.0
+    for i in range(n - k + 1):
+        prob += math.comb(n, k + i) * (p ** (k + i)) * ((1 - p) ** (n - (k + i)))
+    return prob
 
 def disponibilidade_vec(
         n: np.ndarray[int],
@@ -28,11 +28,13 @@ def disponibilidade_vec(
     p - prob de um servidor estar up, 0 <= p <= 1
     X - número de servidores disponíveis em um dado momento
     """
+    assert len(n) == len(k) == len(p), f"n, k e p devem ter o mesmo tamanho. n: {len(n)}, k: {len(k)}, p: {len(p)}"
     prob = np.zeros_like(p)
     for case in range(len(n)):
-        for i in range(n[case] - k[case]):
-            prob[case] += math.comb(n[case], k[case] + i) * p[case] ** (k[case] + i) * (1 - p[case]) ** (n[case] - k[case] + i)
-    return 1.0 - prob
+        for i in range(n[case] - k[case] + 1):
+            # prob[case] += math.comb(n[case], k[case] + i) * (p[case] ** (k[case] + i)) * ((1 - p[case]) ** (n[case] - k[case] + i))
+            prob[case] += math.comb(n[case], k[case] + i) * (p[case] ** (k[case] + i)) * ((1 - p[case]) ** (n[case] - (k[case] + i)))
+    return prob
 
 
 if __name__ == "__main__":
